21c682f837defe0e08352d49bde98a56b3eeacc2
Otabek <otashqashqar@gmail.com>
2025-05-06 11:11:43 +0500

    final version

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -18,7 +18,7 @@ SECRET_KEY = config('DJANGO_SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', default=False, cast=bool)
 
-ALLOWED_HOSTS = ['*.onrender.com', '127.0.0.1']
+ALLOWED_HOSTS = [os.environ.get('RENDER_EXTERNAL_HOSTNAME', '127.0.0.1')]
 
 # Application definition
 
