06807eede448b8c251cbf7093172666f91c0c874
Otabek <otashqashqar@gmail.com>
2025-05-06 10:52:39 +0500

    fanil version

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -69,7 +69,12 @@ WSGI_APPLICATION = 'portfolio.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
-DATABASES = {'default': dj_database_url.parse(os.getenv('DATABASE_URL'))}
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/5.2/ref/settings/#auth-password-validators
